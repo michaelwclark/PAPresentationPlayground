a8db1afbdcf37cf77c04d43a9b1634318378816a
LaRayne Jack <fenton@assess.co.polk.ia.us>
2015-07-09 08:52:20 -0500

    Initial RequireJS work & Project Structure

--- scripts/require-setup.js
+++
@@ -1,9 +0,0 @@
-var require = {
-  shim: {
-    "bootstrap" : { "deps" :['jquery']}
-  },
-  paths:{
-    "jquery" : "//code.jquery.com/jquery-2.1.1.min",
-    "bootstrap" : "bootstrap-3.3.5-dist/js/bootstrap.min.js"
-  }
-};

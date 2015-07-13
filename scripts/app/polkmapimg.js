a012711c5cd6b869757423f6afb3c5bb2b485df4
Mike Clark <fenton@assess.co.polk.ia.us>
2015-07-13 08:47:48 -0500

    Added link to Polk Map Img

--- scripts/app/polkmapimg.js
+++ scripts/app/polkmapimg.js
@@ -51,7 +51,10 @@ function ApplicationUtilities(){
 	};			
 }
 
-
+/// This should go into a common Utils module file.\
+/// Overload generic Object class to include a method for loading arguments
+/// from a map of K:V pairs. This works nested only 1 level deep, but can be modified to work
+/// x levels deep utilizing basic recursion.
 Object.prototype.LoadDynamicArgs = function(args){
 	if(typeof args == 'object' && typeof this == 'object'){
 		var keys = Object.keys(args);
